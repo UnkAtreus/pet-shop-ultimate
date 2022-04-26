843c689853b9ce23a622ac9cf8b6d55aa0c25542
UnkAtreus <unk.atreus@mgmail.com>
2022-04-26 12:17:13 +0700

    init

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+var Adoption = artifacts.require("Adoption");
+
+module.exports = function (deployer) {
+  deployer.deploy(Adoption);
+};
---
+++ truffle-config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  contracts_build_directory: path.join(__dirname, "src/contracts"),
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+    },
+  },
+};
