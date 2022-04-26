7c1fef48765421aee9c90e22767b1d9c3c3a85ee
UnkAtreus <unk.atreus@mgmail.com>
2022-04-26 14:34:34 +0700

    pokemon card

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "src/contracts"),
   networks: {
     development: {
